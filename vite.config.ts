dfdc0cf30f0bb93ef154549a9b7e5b182ac7a492
truckmanjohn725-lgtm <truckmanjohn725@gmail.com>
2026-06-05 22:21:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
     host: true
